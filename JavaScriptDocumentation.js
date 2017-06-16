a5bf4d601f7740eb1e9e2c4d19fc68b9970a871b
LanceDang <lancedang90@gmail.com>
2017-06-15 20:24:16 -0700

    JavaScript and Http Request AJAX

--- JavaScriptDocumentation.js
+++ JavaScriptDocumentation.js
@@ -699,14 +699,40 @@ event.clientY
 
 
 
+JavaScript and Http Requests AJAX
+
+var http = new XMLHttpRequest();
+var url = 'http://jsonplaceholder.typicode.com/posts';
+var method = "GET";
+
+http.open(method, url);
+http.onreadystatechange = function() {
+	if (http.readyState === XMLHttpRequest.DONE && http.status === 200) {// DONE == 4
+		console.log(JSON.parse(http.responseText)[0]);
+	} else if (http.readyState == XMLHttpRequest.DONE && http.status !== 200) {
+		console.log('Error!');
+	}  
+};
+http.send();
 
 
+// POST Request
+var http = new XMLHttpRequest();
+var url = 'http://jsonplaceholder.typicode.com/posts';
+var method = "POST";
 
+var data = 'title=Post%20Title&body=Body';
 
-
-
-
-
+http.open(method, url);
+http.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+http.onreadystatechange = function() {
+	if (http.readyState === XMLHttpRequest.DONE && http.status === 201) {// DONE == 4
+		console.log(JSON.parse(http.responseText));
+	} else if (http.readyState == XMLHttpRequest.DONE && http.status !== 201) {
+		console.log('Error!');
+	}  
+};
+http.send(data);
 
 
 
